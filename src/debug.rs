c03ec586f1e2dc98805eef1fdd31827f8263ed8b
Tyson Liddell <tysonliddell@hotmail.com>
2023-04-08 22:09:24 +0100

    Add I address dereferencing instructions.

--- src/debug.rs
+++ src/debug.rs
@@ -1,6 +1,9 @@
 use crate::{
     interpreter::STACK_POINTER_ADDRESS,
-    memory::{CosmacRAM, PROGRAM_LAST_ADDRESS, PROGRAM_START_ADDRESS, STACK_START_ADDRESS},
+    memory::{
+        CosmacRAM, MEMORY_START_ADDRESS, PROGRAM_LAST_ADDRESS, PROGRAM_START_ADDRESS,
+        STACK_START_ADDRESS,
+    },
 };
 
 pub fn panic_if_pc_address_not_in_chip8_program_range(address: u16) {
@@ -8,6 +11,17 @@ pub fn panic_if_pc_address_not_in_chip8_program_range(address: u16) {
         panic!(
             "Attempt to set program counter to address {address:#X} which is outside of \
             CHIP-8 program address range."
+        );
+    }
+}
+
+pub fn panic_if_i_address_out_of_bounds(address: u16) {
+    // `I` register needs to be able to access character glyphs, which lie before
+    // PROGRAM_START_ADDRESS.
+    if !(MEMORY_START_ADDRESS..=PROGRAM_LAST_ADDRESS).contains(&(address as usize)) {
+        panic!(
+            "Attempt to set I address to {address:#X} which is outside of \
+            normal operating range."
         );
     }
 }
