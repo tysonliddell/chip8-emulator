87a0b1e9d2a8a4d57e8ab1e0a17200275d67776f
Tyson Liddell <tysonliddell@hotmail.com>
2023-04-09 13:23:13 +0100

    Add dummy peripherals.

--- src/peripherals.rs
+++ src/peripherals.rs
@@ -1,3 +1,5 @@
+// use std::cell::Cell;
+
 pub trait Tone {
     fn start_tone(&self);
     fn stop_tone(&self);
@@ -10,4 +12,28 @@ pub trait Screen {
 
 pub trait HexKeyboard {
     fn get_current_pressed_key(&self) -> Option<u8>;
+}
+
+pub struct DummyPeripherals {
+    // tone_is_on: Cell<bool>,
+}
+
+impl Tone for DummyPeripherals {
+    fn is_tone_on(&self) -> bool {
+        false
+    }
+
+    fn start_tone(&self) {}
+
+    fn stop_tone(&self) {}
+}
+
+impl Screen for DummyPeripherals {
+    fn draw_monochrome_scanline(&self, scanline_pixels: &[u8]) {}
+}
+
+impl HexKeyboard for DummyPeripherals {
+    fn get_current_pressed_key(&self) -> Option<u8> {
+        None
+    }
 }
